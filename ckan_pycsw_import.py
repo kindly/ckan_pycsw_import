ece0282d652984f45c80347e3fca123963a588d8
kindly <kindly@gmail.com>
2013-03-31 17:01:11 +0100

    delete not existing records, and change changed ones.

--- ckan_pycsw_import.py
+++ ckan_pycsw_import.py
@@ -8,13 +8,13 @@ import datetime
 import io
 from pycsw import config
 
-logging.basicConfig(format='%(message)s', level=logging.ERROR)
+logging.basicConfig(format='%(message)s', level=logging.INFO)
 
 LOGGER = logging.getLogger(__name__)
 
 def setup_db(database, table):
     """Setup database tables and indexes"""
-    from sqlalchemy import Column, create_engine, Integer, String, MetaData, \
+    from sqlalchemy import Column, create_engine, Integer, MetaData, \
         Table, Text
 
     LOGGER.info('Creating database %s', database)
@@ -60,9 +60,9 @@ def setup_db(database, table):
         Column('organization', Text, index=True),
 
         # security
-        Column('securityconstraints', Text, index=True),
-        Column('accessconstraints', Text, index=True),
-        Column('otherconstraints', Text, index=True),
+        Column('securityconstraints', Text),
+        Column('accessconstraints', Text),
+        Column('otherconstraints', Text),
 
         # date
         Column('date', Text, index=True),
@@ -119,6 +119,7 @@ def load_ckan(database, ckan_url, table):
     LOGGER.info(str(datetime.datetime.now()))
 
     query = 'api/search/dataset?qjson={"fl":"id,metadata_modified,extras_harvest_object_id,extras_metadata_source", "q":"harvest_object_id:*", "limit":1000, "start":%s}'
+
     start = 0
 
     gathered_records = {}
@@ -145,48 +146,87 @@ def load_ckan(database, ckan_url, table):
     LOGGER.info('gather finished')
     LOGGER.info(str(datetime.datetime.now()))
 
-    query = ckan_url + 'harvest/object/%s'
+    existing_records = {}
 
-    for ckan_id, value in gathered_records.iteritems():
-        url = query % value['harvest_object_id']
-        response = requests.get(url)
+    query = repo.session.query(repo.dataset.ckan_id, repo.dataset.ckan_modified)
+    for row in query:
+        existing_records[row[0]] = row[1]
+    repo.session.close()
 
-        if value['source'] == 'arcgis':
+    new = set(gathered_records) - set(existing_records)
+    deleted = set(existing_records) - set(gathered_records)
+    changed = set()
+
+    for key in set(gathered_records) & set(existing_records):
+        if gathered_records[key]['metadata_modified'] > existing_records[key]:
+            changed.add(key)
+
+    for ckan_id in deleted:
+        try:
+            repo.session.begin()
+            rows = repo.session.query(repo.dataset.ckan_id).filter_by(
+            ckan_id=ckan_id).delete()
+            LOGGER.info('Deleted %s' % ckan_id)
+            repo.session.commit()
+        except Exception, err:
+            repo.session.rollback()
+            raise
+
+    for ckan_id in new:
+        ckan_info = gathered_records[ckan_id]
+        record = get_record(context, repo, ckan_url, ckan_id, ckan_info)
+        if not record:
+            LOGGER.info('skipped record %s' % ckan_id)
             continue
-
-        try:
-            xml = etree.parse(io.BytesIO(response.content))
-        except Exception, err:
-            LOGGER.error('Could not pass xml doc from %s, Error: %s' % (ckan_id, err))
-            
-        try:
-            record = metadata.parse_record(context, xml, repo)[0]
-        except Exception, err:
-            LOGGER.error('Could not extract metadata from %s, Error: %s' % (ckan_id, err))
-
-        if not record.identifier:
-            record.identifier = ckan_id
-        record.ckan_id = ckan_id
-        record.ckan_modified = value['metadata_modified']
-
         try:
             repo.insert(record, 'local', util.get_today_and_now())
-            LOGGER.info('Inserted')
+            LOGGER.info('Inserted %s' % ckan_id)
         except Exception, err:
             LOGGER.error('ERROR: not inserted %s Error:%s' % (ckan_id, err))
-        
 
-def load_xml(context, xml, repo):
+    for ckan_id in changed:
+        ckan_info = gathered_records[ckan_id]
+        record = get_record(context, repo, ckan_url, ckan_id, ckan_info)
+        if not record:
+            continue
+        update_dict = dict([(getattr(repo.dataset, key),
+        getattr(record, key)) \
+        for key in record.__dict__.keys() if key != '_sa_instance_state'])
+        try:
+            repo.session.begin()
+            repo.session.query(repo.dataset).filter_by(
+            ckan_id=ckan_id).update(update_dict)
+            repo.session.commit()
+            LOGGER.info('Changed %s' % ckan_id)
+        except Exception, err:
+            repo.session.rollback()
+            raise RuntimeError, 'ERROR: %s' % str(err)
 
-    exml = etree.parse(io.BytesIO(xml))
 
-    record = metadata.parse_record(context, xml, repo)[0]
+def get_record(context, repo, ckan_url, ckan_id, ckan_info):
+    query = ckan_url + 'harvest/object/%s'
+    url = query % ckan_info['harvest_object_id']
+    response = requests.get(url)
+
+    if ckan_info['source'] == 'arcgis':
+        return
 
     try:
-        repo.insert(record, 'local', util.get_today_and_now())
-        LOGGER.info('Inserted')
+        xml = etree.parse(io.BytesIO(response.content))
     except Exception, err:
-        LOGGER.critical('ERROR: not inserted %s', err)
+        LOGGER.error('Could not pass xml doc from %s, Error: %s' % (ckan_id, err))
+
+    try:
+        record = metadata.parse_record(context, xml, repo)[0]
+    except Exception, err:
+        LOGGER.error('Could not extract metadata from %s, Error: %s' % (ckan_id, err))
+
+    if not record.identifier:
+        record.identifier = ckan_id
+    record.ckan_id = ckan_id
+    record.ckan_modified = ckan_info['metadata_modified']
+
+    return record
 
 
 if __name__ == '__main__':
@@ -195,5 +235,10 @@ if __name__ == '__main__':
         setup_db(sys.argv[2], 'records')
 
     if sys.argv[1] == 'load':
-        load_ckan(sys.argv[2], 'http://geo.gov.ckan.org', 'records')
+        load_ckan(sys.argv[2], sys.argv[3], 'records')
+
+
+
+
+
 
